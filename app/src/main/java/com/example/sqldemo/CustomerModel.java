492c0d4667836eb0b8a6035bbb128046315778de
unknown <xoaquin.bg@gmail.com>
2023-09-12 19:09:42 +0200

    Part 3 - Model

---
+++ app/src/main/java/com/example/sqldemo/CustomerModel.java
@@ -0,0 +1,56 @@
+package com.example.sqldemo;
+
+public class CustomerModel {
+
+    private int id;
+    private String name;
+    private int age;
+    private boolean isActive;
+
+    public CustomerModel(int id) {
+        this.id = id;
+    }
+
+    public CustomerModel(int id, String name, int age, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.isActive = isActive;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
--- app/src/main/java/com/example/sqldemo/MainActivity.java
+++ app/src/main/java/com/example/sqldemo/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.sqldemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // references to buttons and other controls (member variables)
+    Button btn_add, btn_viewAll;
+    EditText et_name, et_age;
+    Switch sw_activeCustomer;
+    ListView lv_customerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
+        et_age = findViewById(R.id.et_age);
+        et_name = findViewById(R.id.et_name);
+        sw_activeCustomer = findViewById(R.id.sw_active);
+        lv_customerList = findViewById(R.id.lv_customerList);
+
+        // button listeners
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    CustomerModel customerModel = new CustomerModel(-1,et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
+                    Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+
+                } catch(Exception e) {
+                    Toast.makeText(MainActivity.this, "Error Creating Customer", Toast.LENGTH_SHORT).show();
+                }
+                
+            }
+        });
+
+        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "View All Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
